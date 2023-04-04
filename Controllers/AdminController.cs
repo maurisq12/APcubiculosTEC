fbff8b907780187efbc109579ac38c3d3b8d9a72
Aaron <aaronrs02@hotmail.com>
2023-04-04 00:00:35 -0600

    pagina de edicion de estudiante

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -31,5 +31,14 @@ public class Admin : Controller
         public IActionResult gestTiempos(){
         return View();
     }
+        public IActionResult editEstudiante(){
+        var estudianteEdit = Estudiante.todosEstudiantes()[Int32.Parse(Request.Query["id"])-1];
+        Console.WriteLine(estudianteEdit.getNombre());
+        Console.WriteLine(estudianteEdit.correo);
+        ViewBag.Estudiante = estudianteEdit;
+
+
+        return View();
+    }
 
 }
