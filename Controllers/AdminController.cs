53644f860c5108d44d7ca21100c31a53699bbc5f
Aaron <aaronrs02@hotmail.com>
2023-04-10 22:47:53 -0600

    eliminar estudiante

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,27 +5,31 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace CubiculosTEC.Controllers;
 
-[Authorize(Roles="administrador")]
+[Authorize(Roles = "administrador")]
 public class Admin : Controller
 {
 
     //SET: /Cubi/
-    public IActionResult gestEstudiantes(){
+    public IActionResult gestEstudiantes()
+    {
         var estudiantes = Estudiante.todosEstudiantes();
         ViewBag.Estudiantes = estudiantes;
         return View();
     }
-    public IActionResult gestCubiculos(){
+    public IActionResult gestCubiculos()
+    {
         var cubiculos = Cubiculo.cubiculosTodos();
         ViewBag.Cubiculos = cubiculos;
         return View();
     }
-    public IActionResult gestAsignaciones(){
+    public IActionResult gestAsignaciones()
+    {
         var asignaciones = Reservacion.todasReservaciones();
         ViewBag.Reservas = asignaciones;
         return View();
     }
-    public IActionResult gestTiempos(){
+    public IActionResult gestTiempos()
+    {
         var cubiculos = Cubiculo.cubiculosTodos();
         ViewBag.Cubiculos = cubiculos;
         return View();
@@ -33,48 +37,55 @@ public class Admin : Controller
 
 
 
-    public IActionResult editEstudiante(){
-        var estudianteEdit = Estudiante.todosEstudiantes()[Int32.Parse(Request.Query["id"])-2];
-        estudianteEdit.fechaNacimiento=DateTime.Parse(estudianteEdit.fechaNacimiento).ToString("yyyy-MM-dd");
+    public IActionResult editEstudiante()
+    {
+        var estudianteEdit = Estudiante.todosEstudiantes()[Int32.Parse(Request.Query["id"]) - 2];
+        estudianteEdit.fechaNacimiento = DateTime.Parse(estudianteEdit.fechaNacimiento).ToString("yyyy-MM-dd");
         ViewBag.Estudiante = estudianteEdit;
         return View();
     }
 
-         public IActionResult editCubiculo(){
-        var cubiculoEdit = Cubiculo.cubiculosTodos()[Int32.Parse(Request.Query["id"])-1];
+    public IActionResult editCubiculo()
+    {
+        var cubiculoEdit = Cubiculo.cubiculosTodos()[Int32.Parse(Request.Query["id"]) - 1];
         ViewBag.Cubiculo = cubiculoEdit;
         return View();
     }
 
-    public IActionResult editReserva(){
-        var reservaEdit = Reservacion.todasReservaciones()[Int32.Parse(Request.Query["id"])-1];
-        reservaEdit.fechaDeUso=DateTime.Parse(reservaEdit.fechaDeUso).ToString("yyyy-MM-dd");
-        reservaEdit.fechaDeReservacion=DateTime.Parse(reservaEdit.fechaDeReservacion).ToString("yyyy-MM-dd");
+    public IActionResult editReserva()
+    {
+        var reservaEdit = Reservacion.todasReservaciones()[Int32.Parse(Request.Query["id"]) - 1];
+        reservaEdit.fechaDeUso = DateTime.Parse(reservaEdit.fechaDeUso).ToString("yyyy-MM-dd");
+        reservaEdit.fechaDeReservacion = DateTime.Parse(reservaEdit.fechaDeReservacion).ToString("yyyy-MM-dd");
         ViewBag.Reserva = reservaEdit;
         Console.WriteLine(reservaEdit.horaInicio);
         return View();
     }
 
     [HttpPost]
-    public IActionResult editEstudianteconf(){
-        Estudiante.editarEstudiante(Int32.Parse(Request.Form["laCedula"]),Int32.Parse(Request.Form["elCarne"]),Request.Form["elNombre"],Request.Form["elApellido1"],Request.Form["elApellido2"],Request.Form["laFecha"],Request.Form["elCorreo"],Request.Form["laContrasena"], Int16.Parse(Request.Form["elEstado"]));
+    public IActionResult editEstudianteconf()
+    {
+        Estudiante.editarEstudiante(Int32.Parse(Request.Form["laCedula"]), Int32.Parse(Request.Form["elCarne"]), Request.Form["elNombre"], Request.Form["elApellido1"], Request.Form["elApellido2"], Request.Form["laFecha"], Request.Form["elCorreo"], Request.Form["laContrasena"], Int16.Parse(Request.Form["elEstado"]));
         return View("realizadoAdmin");
 
     }
 
     [HttpPost]
-    public IActionResult editCubiculoconf(){
-        Cubiculo.editarCubiculo(Int32.Parse(Request.Form["elId"]),Request.Form["elNombre"],Int32.Parse(Request.Form["elEstado"]),Int32.Parse(Request.Form["laCapacidad"]));
+    public IActionResult editCubiculoconf()
+    {
+        Cubiculo.editarCubiculo(Int32.Parse(Request.Form["elId"]), Request.Form["elNombre"], Int32.Parse(Request.Form["elEstado"]), Int32.Parse(Request.Form["laCapacidad"]));
         return View("realizadoAdmin");
     }
 
     [HttpPost]
-    public IActionResult editReservaconf(){
-        Reservacion.modificarReservacion(Int32.Parse(Request.Form["elId"]), Int32.Parse(Request.Form["elIdCubiculo"]),Int32.Parse(Request.Form["elIdEstudiante"]),Request.Form["laFechaDeUso"],Request.Form["laHoraInicio"],Request.Form["laHoraFinal"],Request.Form["laFechaDeReserva"],Request.Form["laConfirmacion"]);
+    public IActionResult editReservaconf()
+    {
+        Reservacion.modificarReservacion(Int32.Parse(Request.Form["elId"]), Int32.Parse(Request.Form["elIdCubiculo"]), Int32.Parse(Request.Form["elIdEstudiante"]), Request.Form["laFechaDeUso"], Request.Form["laHoraInicio"], Request.Form["laHoraFinal"], Request.Form["laFechaDeReserva"], Request.Form["laConfirmacion"]);
         return View("realizadoAdmin");
     }
 
-    public IActionResult asignacionesEstudiante(){
+    public IActionResult asignacionesEstudiante()
+    {
         var asignaciones = Reservacion.reservacionesUsuario(Int32.Parse(Request.Query["id"]));
         Console.WriteLine(asignaciones.Count());
         ViewBag.Reservas = asignaciones;
@@ -82,8 +93,9 @@ public class Admin : Controller
     }
 
     [HttpPost]
-    public IActionResult realizarBloqueo(){
-        Cubiculo.bloqueoCubiculo(Int32.Parse(Request.Form["elId"]),Request.Form["laFechaDeUso"].ToString(),Request.Form["laHoraInicio"].ToString(),Request.Form["laHoraFinal"].ToString(),DateTime.Now.ToString("yyyy-MM-dd"));
+    public IActionResult realizarBloqueo()
+    {
+        Cubiculo.bloqueoCubiculo(Int32.Parse(Request.Form["elId"]), Request.Form["laFechaDeUso"].ToString(), Request.Form["laHoraInicio"].ToString(), Request.Form["laHoraFinal"].ToString(), DateTime.Now.ToString("yyyy-MM-dd"));
         Console.WriteLine(Request.Form["laHoraInicio"].ToString());
         Console.WriteLine(Request.Form["laHoraFinal"].ToString());
         return View();
@@ -91,29 +103,33 @@ public class Admin : Controller
 
 
     [HttpPost]
-    public IActionResult definirHoraMaxima(){
-        Cubiculo.tiempoMaximoCubiculo(Int32.Parse(Request.Form["elId"]),Request.Form["elTiempoMaximo"].ToString());
+    public IActionResult definirHoraMaxima()
+    {
+        Cubiculo.tiempoMaximoCubiculo(Int32.Parse(Request.Form["elId"]), Request.Form["elTiempoMaximo"].ToString());
         Console.WriteLine(Int32.Parse(Request.Form["elId"]));
         Console.WriteLine(Request.Form["eltiempoMaximo"].ToString());
         return View();
     }
 
-    public IActionResult gestCubiculoTiempo(){
-        var cubiculoGest = Cubiculo.cubiculosTodos()[Int32.Parse(Request.Query["id"])-1];
+    public IActionResult gestCubiculoTiempo()
+    {
+        var cubiculoGest = Cubiculo.cubiculosTodos()[Int32.Parse(Request.Query["id"]) - 1];
         ViewBag.Cubiculo = cubiculoGest;
         return View();
     }
 
     [HttpPost]
-    public IActionResult cambiarEstado(){
+    public IActionResult cambiarEstado()
+    {
         Console.WriteLine(Int32.Parse(Request.Form["elEstado"]));
         int elEstado = Int32.Parse(Request.Form["elEstado"]);
         int elId = Int32.Parse(Request.Form["elId"]);
         Cubiculo.cambiarEstadoCubiculo(elId, elEstado);
-        return RedirectToAction("gestTiempos","Admin");
+        return RedirectToAction("gestTiempos", "Admin");
     }
 
-    public IActionResult eliminarCubiculo(){
+    public IActionResult eliminarCubiculo()
+    {
         int IdCubiculo = Int32.Parse(Request.Form["idEliminar"]);
         Cubiculo.eliminarCubiculo(IdCubiculo);
         var cubiculos = Cubiculo.cubiculosTodos();
@@ -121,9 +137,17 @@ public class Admin : Controller
         return View("gestCubiculos");
     }
 
+    public IActionResult eliminarEstudiante()
+    {
+        Estudiante.eliminarEstudiante(Int32.Parse(Request.Form["idEliminar"]));
+        var estudiantes = Estudiante.todosEstudiantes();
+        ViewBag.Estudiantes = estudiantes;
+        return View("gestEstudiantes");
+    }
 
 
-    
+
+
 
 
 
